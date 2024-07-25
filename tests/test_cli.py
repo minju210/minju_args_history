47b97e2d3d33c9cfaa69383a6ed115339da59205
minju210 <juliana6190210@gmail.com>
2024-07-25 15:42:22 +0900

    add

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,5 @@
-#import minju_args_history.cli() import hello_msg
 from minju_args_history.cli import hello_msg
 
 def test_hello():
-    m = hello_msg() 
+    m = hello_msg()
     assert m == "hello"
---
+++ tests/test_utils.py
@@ -0,0 +1,7 @@
+#import minju_args_history.cli() import hello_msg
+from minju_args_history.db.utils import read_data
+import pandas as pd
+
+def test_read_data():
+    i = read_data() 
+    assert isinstance(r, pd.DataFrame)
